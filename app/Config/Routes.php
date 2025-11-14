d5857fe2db25b361ad3ac6dc0f9199f3a00d6e1d
pinkkiwillie <pinkkiiwillyanthi@gmail.com>
2025-11-14 10:01:22 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'Auth::login');
+$routes->get('/register', 'Auth::register');
+$routes->get('/profile', 'User::profile');
